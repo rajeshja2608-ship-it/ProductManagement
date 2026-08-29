87194444759ce092a3b8ff5e086cdfaf67e03644
Rajesh.Jadhav <Rajesh.Jadhav@RAJESH-JADHAVNE>
2026-08-29 16:21:05 +0530

    Addded Teacher API Controller

---
+++ ProductManagement.API/Controllers/TeacherController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProductManagement.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TeacherController : ControllerBase
+    {
+        public IActionResult Get()
+        {
+            return Ok(new[]
+            {
+                new {Id=1,Name="Teacher 1", Subject="Maths"},
+                new {Id=2,Name="Teacher 2", Subject="Science"}
+            });
+        }
+    }
+}
